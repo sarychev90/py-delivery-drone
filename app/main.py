8fc9ad756bd2259cdb7df7dff65a60155b25f441
sarychev90 <dimasarychev90@gmail.com>
2026-08-18 21:36:13 +0300

    Fixed flake8 3

--- app/main.py
+++ app/main.py
@@ -52,12 +52,14 @@ class DeliveryDrone(FlyingRobot):
             name: str,
             weight: int,
             max_load_weight: int,
-            current_load: Cargo,
+            current_load: Cargo | None = None,
             coords: list[int] | None = None
     ) -> None:
         super().__init__(name, weight, coords or [0, 0, 0])
         self.max_load_weight = max_load_weight
         self.current_load = current_load
+        if current_load is not None:
+            self.hook_load(current_load)
 
     def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and self.max_load_weight >= cargo.weight:
